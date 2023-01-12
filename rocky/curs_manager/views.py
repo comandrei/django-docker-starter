3f278aeea65324369301d6d58d3c448f5fadb63a
Andrei Coman <comandrei@gmail.com>
2023-01-12 20:04:05 +0200

    Display students in template

--- rocky/curs_manager/views.py
+++ rocky/curs_manager/views.py
@@ -10,17 +10,12 @@ def salut(request):
     return HttpResponse("salutare")
 
 def studenti(request):
-    lista_studenti = ["Student 1", "Student 2", "Student3"]
     lista_studenti = Student.objects.all()
-    #studenti_formatat = ", ".join(lista_studenti)
-    #studenti_formatat = "<br /> ".join(lista_studenti)
-    studenti_formatat = [(student.nume, student.prenume) for student in lista_studenti]
-    #studenti_formatat = "<br /> ".join(studenti_formatat)
-    output_string = ""
-    for student in lista_studenti:
-        rand = f"{student.nume} {student.prenume} <br />"
-        output_string += rand
-    return HttpResponse(output_string)
+    context = {
+        "studenti": lista_studenti
+    }
+    return render(request, "studenti.html", context)
+
 
 
 def contact(request):
