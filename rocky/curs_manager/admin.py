4d12148c275b243bee3dccbf0aec5d82005b3167
Andrei Coman <comandrei@gmail.com>
2023-01-17 19:56:53 +0200

    Better admin filtering

--- rocky/curs_manager/admin.py
+++ rocky/curs_manager/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Student
 # Register your models here.
-admin.site.register(Student)
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("nume", "prenume", "an")
+    list_filter = ("an", )
+    search_fields = ("nume", "prenume")
+
+admin.site.register(Student, StudentAdmin)
