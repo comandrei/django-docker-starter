d04841615f3ee468826a59501fe3771a72800efc
Andrei Coman <comandrei@gmail.com>
2023-01-17 19:40:17 +0200

    Add default field

--- rocky/curs_manager/models.py
+++ rocky/curs_manager/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Student(models.Model):
     nume = models.TextField()
     prenume = models.TextField()
-    an = models.IntegerField()
+    an = models.IntegerField(default=1)
     telefon = models.TextField()
