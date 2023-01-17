8d25f1add0feea55b39afcb28e203dd4e3225412
Andrei Coman <comandrei@gmail.com>
2023-01-17 20:03:01 +0200

    Add index for an

--- rocky/curs_manager/models.py
+++ rocky/curs_manager/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Student(models.Model):
     nume = models.TextField()
     prenume = models.TextField()
-    an = models.IntegerField(default=1)
+    an = models.IntegerField(default=1, db_index=True)
     telefon = models.TextField(null=True, blank=True)
