b87b9963af65387c66c358af1d13b257245ef188
Andrei Coman <comandrei@gmail.com>
2023-01-17 20:21:12 +0200

    Validator example

--- rocky/curs_manager/models.py
+++ rocky/curs_manager/models.py
@@ -1,6 +1,11 @@
+from django.core.exceptions import ValidationError
 from django.core.validators import MinLengthValidator
 from django.db import models
 
+
+def validate_no_a(value):
+    if "a" in value:
+        raise ValidationError("Caracter nepermis")
 
 # Create your models here.
 class Student(models.Model):
@@ -9,4 +14,5 @@ class Student(models.Model):
     an = models.IntegerField(default=1, db_index=True)
     telefon = models.TextField(null=True, blank=True)
     cnp = models.CharField(unique=True, null=True, blank=True, max_length=13,
-                                validators=[MinLengthValidator(13)])
+                                validators=[MinLengthValidator(13), validate_no_a])
+            
