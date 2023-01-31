c9f9dfb67d04647fae77650e5fd18b59d1106069
Andrei Coman <comandrei@gmail.com>
2023-01-31 20:22:10 +0200

    Custom error message

--- rocky/curs_manager/forms.py
+++ rocky/curs_manager/forms.py
@@ -5,9 +5,9 @@ class ContactForm(forms.Form):
     nume = forms.CharField()
     email = forms.EmailField()
     mesaj = forms.CharField()
-    multumit = forms.BooleanField()
+    multumit = forms.BooleanField(required=False)
 
     def clean_email(self):
         value = self.cleaned_data["email"]
         if not value.endswith("@gmail.com"):
-            raise ValidationError("Eroare")
+            raise ValidationError("Trebuie sa fie o adresa gmail")
--- rocky/curs_manager/views.py
+++ rocky/curs_manager/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.db.models import F
 
@@ -35,16 +35,14 @@ def contact(request):
     return render(request, "public/contact.html")
 
 def contact_2(request):
-    mesaj = None
     if request.method == "POST":
         formular = ContactForm(request.POST)
         if formular.is_valid():
-            mesaj = "Multumim"
+            return redirect("/cursuri")
     else:
         formular = ContactForm()
     context = {
         "formular": formular,
-        "mesaj": mesaj
     }
     return render(request, "contact.html", context)
 
