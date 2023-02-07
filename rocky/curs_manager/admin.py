98bb6ac9b8a754f4bd3bb83f90313b30d2ca3a0b
Andrei Coman <comandrei@gmail.com>
2023-02-07 19:51:02 +0200

    Action example

--- rocky/curs_manager/admin.py
+++ rocky/curs_manager/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-
+from django.db.models import F
 from .models import Student, Adresa, AdresaNoua, StudentProfile, Curs
 # Register your models here.
+
+def promovare(modeladmin, request, queryset):
+    queryset.update(an=F('an') + 1)
+    # Varianta cu mai multe query-uri
+    # for student in queryset:
+    #     student.an = student.an + 1
+    #     student.save()
+        #print(student)
+
 
 class StudentAdmin(admin.ModelAdmin):
     list_display = ("nume", "prenume", "an")
@@ -9,6 +18,7 @@ class StudentAdmin(admin.ModelAdmin):
     list_per_page = 3
     # Student.objects.filter(Q(nume__icontains=request.GET["q"]) | Q(prenume__icontains=request.GET["q"]))
     search_fields = ("nume", "prenume")
+    actions = (promovare, )
 
 admin.site.register(Student, StudentAdmin)
 
