cd01cb699b6176dbf92ed284c0962852c68bf2dc
Andrei Coman <comandrei@gmail.com>
2023-02-09 20:16:15 +0200

    Add to admin

--- rocky/app2/admin.py
+++ rocky/app2/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
 # Register your models here.
+
+from .models import Intrebare, VariantaRaspuns
+
+admin.site.register(Intrebare)
+admin.site.register(VariantaRaspuns)
