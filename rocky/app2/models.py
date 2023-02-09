ef8bb042b2efc241b2d3e0bac00ae4a54e16d232
Andrei Coman <comandrei@gmail.com>
2023-02-09 20:16:53 +0200

    Add right labels

--- rocky/app2/models.py
+++ rocky/app2/models.py
@@ -3,10 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Intrebare(models.Model):
+    class Meta:
+        verbose_name_plural = "Intrebari"
+
     text = models.TextField()
     punctaj = models.FloatField()
 
 class VariantaRaspuns(models.Model):
+    class Meta:
+        verbose_name_plural = "Variante Raspuns"
     intrebare = models.ForeignKey(Intrebare, on_delete=models.CASCADE)
     corect = models.BooleanField(default=False)
     feedback = models.CharField(max_length=50, null=True, blank=True)
