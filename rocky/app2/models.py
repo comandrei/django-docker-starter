08e7b3af2a1aafbd1c085223ca1a26ab806ffbb4
Andrei Coman <comandrei@gmail.com>
2023-02-09 20:13:54 +0200

    Add models for quiz

--- rocky/app2/models.py
+++ rocky/app2/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Intrebare(models.Model):
+    text = models.TextField()
+    punctaj = models.FloatField()
+
+class VariantaRaspuns(models.Model):
+    intrebare = models.ForeignKey(Intrebare, on_delete=models.CASCADE)
+    corect = models.BooleanField(default=False)
+    feedback = models.CharField(max_length=50, null=True, blank=True)
