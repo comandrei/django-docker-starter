e3cd40209cd08031b40a7f7cb6da7147c4caf192
Andrei Coman <comandrei@gmail.com>
2023-02-21 21:10:21 +0200

    Pagination

--- rocky/rocky/settings.py
+++ rocky/rocky/settings.py
@@ -61,6 +61,11 @@ MIDDLEWARE = [
 
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 ROOT_URLCONF = "rocky.urls"
 
 TEMPLATES = [
