c2013199258005aacf48a1784af4f0429e61a34e
Andrei Coman <comandrei@gmail.com>
2023-02-21 19:10:51 +0200

    Student export example

--- rocky/curs_manager/management/commands/export_studenti.py
+++ rocky/curs_manager/management/commands/export_studenti.py
@@ -1,5 +1,24 @@
+import csv
 from django.core.management.base import BaseCommand
+
+from curs_manager.models import Student
+
+def exporta_studenti(student_qs):
+    with open("students.csv", "w") as f:
+        fieldnames = ["nume", "prenume", "an", "cnp"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for student in student_qs:
+            #print(type(student), student.nume, student.prenume)
+            student_row = {
+                "nume": student.nume,
+                "prenume": student.prenume,
+                "an": student.an,
+                "cnp": student.cnp 
+            }
+            writer.writerow(student_row)
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
-        print("Salut!")
+        studenti = Student.objects.all()
+        exporta_studenti(studenti)
